0d62b7559eb846be5b01f9e7cafd70e98d1e1910
Zied Bouazizi <zied.bouazizi@outlook.com>
2023-02-21 19:40:25 +0100

    Install Vuex & create store

---
+++ backend/src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+    state: {
+        test: 'test msg'
+    },
+    getters: {},
+    actions: {},
+    mutations: {},
+})
+
+export default store
