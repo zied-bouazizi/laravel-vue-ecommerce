da2d0c286d1b8cb3f5b0c549965c329a732af6ae
Zied Bouazizi <zied.bouazizi@outlook.com>
2023-02-22 10:58:32 +0100

    Create app layout

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,35 +1,67 @@
 import { createRouter, createWebHashHistory } from "vue-router";
+import AppLayout from '../components/AppLayout.vue';
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
 import RequestPassword from "../views/RequestPassword.vue";
 import ResetPassword from "../views/ResetPassword.vue";
+import store from "../store";
 
 const routes = [
     {
-        path: '/dashboard',
-        name: 'dashboard',
-        component: Dashboard
+        path: '/app',
+        name: 'app',
+        redirect: '/app/dashboard',
+        component: AppLayout,
+        meta: {
+            requiresAuth: true
+        },
+        children: [
+            {
+                path: 'dashboard',
+                name: 'app.dashboard',
+                component: Dashboard
+            },
+        ]
     },
     {
         path: '/login',
         name: 'login',
-        component: Login
+        component: Login,
+        meta: {
+            requiresGuest: true
+        }
     },
     {
         path: '/request-password',
         name: 'requestPassword',
-        component: RequestPassword
+        component: RequestPassword,
+        meta: {
+            requiresGuest: true
+        }
     },
     {
         path: '/reset-password',
         name: 'ResetPassword',
-        component: ResetPassword
+        component: ResetPassword,
+        meta: {
+            requiresGuest: true
+        }
     },
 ];
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.state.user.token) {
+        next({ name: 'login' })
+    } else if (to.meta.requiresGuest && store.state.user.token) {
+        next({ name: 'app.dashboard' })
+    } else {
+        next();
+    }
 })
 
 export default router;
